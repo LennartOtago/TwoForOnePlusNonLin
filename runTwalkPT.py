bd1bec3a352a63545cb42d31662dd350525b1495
LennartGol <golksart@gmail.com>
2025-03-05 09:24:43 +1300

    small updated

--- runTwalkPT.py
+++ runTwalkPT.py
@@ -41,6 +41,14 @@ def set_size(width, fraction=1):
 
     return fig_dim
 
+def pressFunc(x, b1, b2, h0, p0):
+    b = np.ones(len(x))
+    b[x<=h0] = b1
+    b[x>h0] = b2
+
+    return -b * (x - h0) + np.log(p0)
+
+
 def temp_func(x,h0,h1,h2,h3,h4,h5,a0,a1,a2,a3,a4,b0):
     a = np.ones(x.shape)
     b = np.ones(x.shape)
@@ -67,6 +75,81 @@ def temp_func(x,h0,h1,h2,h3,h4,h5,a0,a1,a2,a3,a4,b0):
     h[h4 <= x] = h4
     h[h5 <= x] = h5
     return a * (x - h) + b
+
+
+def ErrorPressFuncTemp(x,h0,h1,h2,h3,h4,h5,a0,a1,a2,a3,a4,b0, b1, b2, hp0, p0, Var):
+    b = np.ones(len(x))
+    varB = np.ones(len(x))
+    b[x<=hp0] = b1
+    b[x>hp0] = b2
+
+    varB[x<=hp0] = Var[0]
+    varB[x>hp0] = Var[1]
+    F = np.exp(pressFunc(x, b1, b2, hp0, p0))/temp_func(x,h0,h1,h2,h3,h4,h5,a0,a1,a2,a3,a4,b0)
+    return ((x-hp0)* F)**2 * varB + ( b * F)**2 * Var[2] +  (np.exp(-b * (x - hp0) )//temp_func(x,h0,h1,h2,h3,h4,h5,a0,a1,a2,a3,a4,b0))**2 * Var[3]
+
+def ErrorPressFunc(x, b1, b2, hp0, p0, Var):
+    b = np.ones(len(x))
+    varB = np.ones(len(x))
+    b[x<=hp0] = b1
+    b[x>hp0] = b2
+
+    varB[x<=hp0] = Var[0]
+    varB[x>hp0] = Var[1]
+
+    F = np.exp(pressFunc(x, b1, b2, hp0, p0))
+    return ((x-hp0)* F)**2 * varB+ ( b * F)**2 * Var[2] + np.exp(-b * (x - hp0) ) **2 * Var[3]
+
+
+def ErrorOneOverTempFunc(x, h0, h1, h2, h3, h4, h5, a0, a1, a2, a3, a4, b0, b1, b2, hp0, p0, Var):
+
+    ErrorMat = np.zeros((x.shape[0],12))
+    pTsq =  np.exp(pressFunc(x, b1, b2, hp0, p0)) / temp_func(x, h0, h1, h2, h3, h4, h5, a0, a1, a2, a3, a4, b0) ** 2
+
+
+    # h0
+    ErrorMat[x < h0, 0] = 0
+    ErrorMat[h0 <= x, 0] = a0 * (pTsq[h0 <= x])
+    # h1
+    ErrorMat[x < h1, 1] = 0
+    ErrorMat[h1 <= x, 1] = a1 * (pTsq[h1 <= x])
+    # h2
+    ErrorMat[x < h2, 2] = 0
+    ErrorMat[h2 <= x, 2] = (a1 - a2) * (pTsq[h2 <= x])
+    # h3
+    ErrorMat[x < h3, 3] = 0
+    ErrorMat[h3 <= x, 3] = a2 * (pTsq[h3 <= x])
+    # h4
+    ErrorMat[x < h4, 4] = 0
+    ErrorMat[h4 <= x, 4] = a3 * (pTsq[h4 <= x])
+    # h5
+    ErrorMat[x < h5, 5] = 0
+    ErrorMat[h5 <= x, 5] = (a3-a4) * (pTsq[h5 <= x])
+
+    # a0
+    ErrorMat[x < h1, 6] = x[x < h1]
+    ErrorMat[h1 <= x, 6] = h0 * (pTsq[h1 <= x])
+    # a1
+    ErrorMat[x < h1, 7] = 0
+    ErrorMat[h1 <= x, 7] = (x[h1 <= x] - h1) * (pTsq[h1 <= x])
+    ErrorMat[h2 <= x, 7] = (h2 - h1) * (pTsq[h2 <= x])
+    # a2
+    ErrorMat[x < h2, 8] = 0
+    ErrorMat[h2 <= x, 8] = (x[h2 <= x] - h2) * (pTsq[h2 <= x])
+    ErrorMat[h3 <= x, 8] = (h3 - h2) * (pTsq[h3 <= x])
+    # a3
+    ErrorMat[x < h4, 9] = 0
+    ErrorMat[h4 <= x, 9] = (x[h4 <= x] - h4) * (pTsq[h4 <= x])
+    ErrorMat[h5 <= x, 9] = (h5 - h4) * (pTsq[h5 <= x])
+    # a4
+    ErrorMat[x < h5, 10] = 0
+    ErrorMat[h5 <= x, 10] = (x[h5 <= x] - h5) * (pTsq[h5 <= x])
+
+    # b0* (pTsq[h5 <= x])
+    ErrorMat[:, 11] = 1* (pTsq)
+
+
+    return ErrorMat**2 @ Var
 
 def g(A, L, l):
     """ calculate g"""
@@ -113,7 +196,7 @@ import numpy as np
 
 
 dir = '/home/lennartgolks/PycharmProjects/firstModelCheckPhD/'
-#dir = '/Users/lennart/PycharmProjects/firstModelCheckPhD/'
+dir = '/Users/lennart/PycharmProjects/firstModelCheckPhD/'
 #dir = '/Users/lennart/PycharmProjects/TTDecomposition/'
 B_inv_A_trans_y0 = np.loadtxt(dir + 'B_inv_A_trans_y0.txt')
 VMR_O3 = np.loadtxt(dir + 'VMR_O3.txt')
@@ -158,7 +241,7 @@ plt.show()
 fig3, ax1 = plt.subplots(tight_layout = True,figsize=set_size(245, fraction=fraction))
 ax1.plot(pressure_values.reshape((SpecNumLayers,1))/temp_values.reshape((SpecNumLayers,1)), height_values)
 ax2 = ax1.twiny()
-#ax1.plot(1/temp_values.reshape((SpecNumLayers,1)), height_values)
+ax2.plot(1/temp_values.reshape((SpecNumLayers,1)), height_values)
 #ax2.plot(pressure_values.reshape((SpecNumLayers,1)), height_values)
 #ax1.set_xscale('log')
 plt.show()
@@ -195,11 +278,6 @@ print('delta:' + str(lam0*gammaMin0))
 print('gamma:' + str(gammaMin0))
 ##
 
-def pressFunc(x, b1, b2, h0, p0):
-    b = np.ones(len(x))
-    b[x>h0] = b2
-    b[x<=h0] = b1
-    return -b * (x - h0) + np.log(p0)
 
 popt, pcov = scy.optimize.curve_fit(pressFunc, height_values[:,0], np.log(pressure_values), p0=[-2e-2,-2e-2, 18, 15])
 
@@ -286,6 +364,7 @@ def log_postTP(params, means, sigmas, popt, A, y, height_values, gamma0):
                 - ((popt[2] - h0P) / sigmaH) ** 2
 
     return postDat + 0.5 * Values
+##
 means = np.zeros(16)
 sigmas = np.zeros(16)
 
@@ -306,35 +385,61 @@ means[13] = popt[1]
 means[14] = popt[2]
 means[15] = popt[3]
 
-sigmas[0] = 0.5 #* 0.1
+sigmas[0] = 2#0.5 #* 0.1
 sigmas[1] = 3 #* 0.1
-sigmas[2] = 1 #* 0.1
-sigmas[3] = 2 #* 0.1
-sigmas[4] = 2 #* 0.1
-sigmas[5] = 2 #* 0.1
-sigmas[6] = 0.01 #* 0.1
-sigmas[7] = 0.01
-sigmas[8] = 0.1
-sigmas[9] = 0.01
-sigmas[10] = 0.01
-sigmas[11] = 2 #* 0.1
+sigmas[2] = 3#1 #* 0.1
+sigmas[3] = 2.5 #* 0.1
+sigmas[4] = 2.5 #* 50
+sigmas[5] = 7 #* 10
+sigmas[6] = 0.01 * 10
+sigmas[7] = 0.01 * 100#00
+sigmas[8] = 0.1 * 100
+sigmas[9] = 0.01 * 100
+sigmas[10] = 0.01 * 100
+sigmas[11] = 2 * 3
 
 #sigmas[0:12] = sigmas[0:12] * 0.02
 
 
-sigmaP = 0.25
-sigmaH = 0.5
-sigmaGrad1 = 0.005
-sigmaGrad2 = 0.01
+sigmaP = 0.25*100
+sigmaH = 0.5 * 3
+sigmaGrad1 = 0.005 * 1
+sigmaGrad2 = 0.01 * 1
 
 sigmas[12] = sigmaGrad1
 sigmas[13] = sigmaGrad2
 sigmas[14] = sigmaH
 sigmas[15] = sigmaP
 #
-#sigmas =10 * sigmas
-means = means - 0.5
+#sigmas =  10 * np.copy(sigmas)
+#means = means - 0.5
 newAPT = RealMap @ APressTemp
+
+fig3, ax1 = plt.subplots(tight_layout = True,figsize=set_size(245, fraction=fraction))
+ErrorsPT = np.sqrt( ErrorOneOverTempFunc(height_values[:,0], *means, sigmas[:12]**2) + ErrorPressFuncTemp(height_values[:,0],  *means, sigmas[12:]**2))
+ax1.errorbar(pressure_values.reshape((SpecNumLayers))/temp_values.reshape((SpecNumLayers)), height_values, xerr = ErrorsPT )
+plt.show()
+
+
+fig3, ax1 = plt.subplots(tight_layout = True,figsize=set_size(245, fraction=fraction))
+Errors = np.sqrt( ErrorPressFunc(height_values[:,0],  *means[12:], sigmas[12:]**2))
+ax1.errorbar(pressure_values.reshape((SpecNumLayers)), height_values, xerr = Errors )
+plt.show()
+
+
+fig3, ax1 = plt.subplots(tight_layout = True,figsize=set_size(245, fraction=fraction))
+x = np.linspace(5,90,100)
+ax1.plot(np.exp(-0.5 * (x - means[0])**2 / sigmas[0] **2))
+ax1.plot(np.exp(-0.5 * (x - means[1])**2 / sigmas[1] **2))
+ax1.plot(np.exp(-0.5 * (x - means[2])**2 / sigmas[2] **2))
+ax1.plot(np.exp(-0.5 * (x - means[3])**2 / sigmas[3] **2))
+ax1.plot(np.exp(-0.5 * (x - means[4])**2 / sigmas[4] **2))
+ax1.plot(np.exp(-0.5 * (x - means[5])**2 / sigmas[5] **2))
+plt.show()
+
+
+
+##
 
 log_post = lambda params: -log_postTP(params, means, sigmas, popt, newAPT, y, height_values, gamma0)
 
@@ -349,7 +454,7 @@ for i in range(0, dim):
     print(univarGrid[i][0])
     print(univarGrid[i][-1])
 gridSize =100
-factor= 75
+factor= 5
 univarGrid = [np.linspace(means[0] - sigmas[0] *factor, means[0] + sigmas[0] *  factor, gridSize),
               np.linspace(means[1] - sigmas[1] * factor, means[1] + sigmas[1] *factor, gridSize),
               np.linspace(means[2] - sigmas[2] * factor, means[2] + sigmas[2] * factor, gridSize),
@@ -369,25 +474,29 @@ univarGrid = [np.linspace(means[0] - sigmas[0] *factor, means[0] + sigmas[0] *  
 
 def MargPostSupp(Params):
     list = []
-    list.append( Params[5] >Params[4] >Params[3] >Params[2] >Params[1] >Params[0] )
-    list.append(univarGrid[1][-1] > Params[1] > univarGrid[1][0])
-    list.append(univarGrid[2][-1] > Params[2] > univarGrid[2][0])
-    list.append(univarGrid[3][-1] > Params[3] > univarGrid[3][0])
-    list.append(univarGrid[4][-1] > Params[4] > univarGrid[4][0])
-    list.append(univarGrid[5][-1] > Params[5] > univarGrid[5][0])
-    list.append(univarGrid[6][0] < Params[6] < univarGrid[6][-1])
-    list.append(univarGrid[7][-1] > Params[7] > univarGrid[7][0])
-    list.append(univarGrid[8][-1] > Params[8] > univarGrid[8][0])
-    list.append(univarGrid[9][0] < Params[9] < univarGrid[9][-1])
-    list.append(univarGrid[10][0] < Params[10] < univarGrid[10][-1])
-    list.append(univarGrid[11][-1] > Params[11] > univarGrid[11][0])
-    list.append(univarGrid[12][-1] > Params[12] > univarGrid[12][0])
-    list.append(univarGrid[13][-1] > Params[13] > univarGrid[13][0])
-    list.append(univarGrid[14][-1] >Params[14] > univarGrid[14][0])
-    list.append(univarGrid[15][-1] >Params[15] > univarGrid[15][0])
-    list.append((320>temp_func(height_values, *Params[:12])).all())
-    list.append((temp_func(height_values, *Params[:12]) > 120).all())
     return all(list)
+
+# def MargPostSupp(Params):
+#     list = []
+#     #list.append( Params[5] >Params[4] >Params[3] >Params[2] >Params[1] >Params[0] )
+#     list.append(univarGrid[1][-1] > Params[1] > univarGrid[1][0])
+#     list.append(univarGrid[2][-1] > Params[2] > univarGrid[2][0])
+#     list.append(univarGrid[3][-1] > Params[3] > univarGrid[3][0])
+#     list.append(univarGrid[4][-1] > Params[4] > univarGrid[4][0])
+#     list.append(univarGrid[5][-1] > Params[5] > univarGrid[5][0])
+#     list.append(univarGrid[6][0] < Params[6] < univarGrid[6][-1])
+#     list.append(univarGrid[7][-1] > Params[7] > univarGrid[7][0])
+#     list.append(univarGrid[8][-1] > Params[8] > univarGrid[8][0])
+#     list.append(univarGrid[9][0] < Params[9] < univarGrid[9][-1])
+#     list.append(univarGrid[10][0] < Params[10] < univarGrid[10][-1])
+#     list.append(univarGrid[11][-1] > Params[11] > univarGrid[11][0])
+#     list.append(univarGrid[12][-1] > Params[12] > univarGrid[12][0])
+#     list.append(univarGrid[13][-1] > Params[13] > univarGrid[13][0])
+#     list.append(univarGrid[14][-1] >Params[14] > univarGrid[14][0])
+#     list.append(univarGrid[15][-1] >Params[15] > univarGrid[15][0])
+#     list.append((320>temp_func(height_values, *Params[:12])).all())
+#     list.append((temp_func(height_values, *Params[:12]) > 120).all())
+#     return all(list)
 # def MargPostSupp(Params):
 #     list = []
 #     list.append(Params[0] > 0)
@@ -414,7 +523,7 @@ x0 = means
 xp0 = 0.9999999 * x0
 dim = len(x0)
 burnIn = 10000
-tWalkSampNum = 3000000
+tWalkSampNum = 100000
 MargPost = pytwalk.pytwalk(n=dim, U=log_post, Supp=MargPostSupp)
 
 #print(" Support of Starting points:" + str(MargPostSupp(x0)) + str(MargPostSupp(xp0)))
@@ -516,6 +625,7 @@ axs.set_xlabel(r'pressure in hPa')
 axs.set_ylabel(r'height in km')
 plt.savefig('PressPostMeanSigm.svg')
 plt.show()
+
 fig, axs = plt.subplots( figsize=set_size(PgWidthPt, fraction=fraction), tight_layout = True,)
 axs.plot( temp_values,height_values,marker = 'o',markerfacecolor = TrueCol, color = TrueCol , label = 'true profile', zorder=0 ,linewidth = 1.5, markersize =7)
 for r in range(0, tests):
@@ -527,6 +637,35 @@ axs.set_xlabel(r'temperature in K ')
 
 axs.set_ylabel(r'height in km')
 plt.savefig('TempPostMeanSigm.svg')
+plt.show()
+
+fig, axs = plt.subplots( figsize=set_size(PgWidthPt, fraction=fraction), tight_layout = True,)
+axs.plot( temp_values,height_values,marker = 'o',markerfacecolor = TrueCol, color = TrueCol , label = 'true profile', zorder=0 ,linewidth = 1.5, markersize =7)
+for r in range(0, tests):
+
+    Sol = temp_func(height_values[:,0], *SampParas[indcies[r], :12])
+    axs.plot(1/ Sol ,height_values , markeredgecolor ='k', color = 'k' ,zorder=3, marker = '.', markersize =2, linewidth =0.5)
+
+axs.set_xlabel(r'temperature in 1/K ')
+
+axs.set_ylabel(r'height in km')
+#plt.savefig('TempPostMeanSigm.svg')
+plt.show()
+
+##
+fig, axs = plt.subplots( figsize=set_size(PgWidthPt, fraction=fraction), tight_layout = True,)
+axs.plot( pressure_values.reshape((SpecNumLayers))/temp_values.reshape((SpecNumLayers)),height_values,marker = 'o',markerfacecolor = TrueCol, color = TrueCol , label = 'true profile', zorder=0 ,linewidth = 1.5, markersize =7)
+for r in range(0, tests):
+    SolP = np.exp(pressFunc(height_values[:, 0], *SampParas[indcies[r], 12:- 1]))
+    SolT = temp_func(height_values[:,0], *SampParas[indcies[r], :12])
+    axs.plot( SolP/SolT ,height_values , markeredgecolor ='k', color = 'k' ,zorder=3, marker = '.', markersize =2, linewidth =0.5)
+
+axs.errorbar(pressure_values.reshape((SpecNumLayers))/temp_values.reshape((SpecNumLayers)), height_values, xerr = ErrorsPT )
+
+axs.set_xlabel(r'pressureover temperature in hPa/K ')
+
+axs.set_ylabel(r'height in km')
+plt.savefig('TempOverPostMeanSigm.svg')
 plt.show()
 
 
